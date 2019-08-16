ce06b6a6392cefe3ae4feafd441302c6af1b405a
Paulo Sergio <psergio348@gmail.com>
2019-08-15 22:55:23 -0300

    Adicionando novas classes e editando View

--- AspNetMVC/AspNetMVC/Data/AspNetMVCContext.cs
+++ AspNetMVC/AspNetMVC/Data/AspNetMVCContext.cs
@@ -13,6 +13,9 @@ namespace AspNetMVC.Models
         {
         }
 
-        public DbSet<AspNetMVC.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+
     }
 }
---
+++ AspNetMVC/AspNetMVC/Migrations/20190816010648_Entities.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AspNetMVC.Migrations
+{
+    public partial class Entities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Seller",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    BirthDate = table.Column<DateTime>(nullable: false),
+                    BaseSalary = table.Column<double>(nullable: false),
+                    DepartmentID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Seller", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Seller_Department_DepartmentID",
+                        column: x => x.DepartmentID,
+                        principalTable: "Department",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SalesRecord",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Amount = table.Column<double>(nullable: false),
+                    Status = table.Column<int>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false),
+                    SellerID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SalesRecord", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_SalesRecord_Seller_SellerID",
+                        column: x => x.SellerID,
+                        principalTable: "Seller",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SalesRecord_SellerID",
+                table: "SalesRecord",
+                column: "SellerID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seller_DepartmentID",
+                table: "Seller",
+                column: "DepartmentID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SalesRecord");
+
+            migrationBuilder.DropTable(
+                name: "Seller");
+
+            migrationBuilder.DropTable(
+                name: "Department");
+        }
+    }
+}
